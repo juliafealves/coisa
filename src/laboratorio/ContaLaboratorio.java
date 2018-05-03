d45967c18bc31eb615476a0013f02d64adace2dc
juliafealves <juliafealves>
2018-05-02 22:59:20 -0300

    Comentários Java Doc.

--- src/laboratorio/ContaLaboratorio.java
+++ src/laboratorio/ContaLaboratorio.java
@@ -4,28 +4,36 @@ package laboratorio;
  * Laboratório de Programação 2 - Lab 2
  * @author Júlia Fernandes Alves (117211383)
  * julia.alves@ccc.ufcg.edu.br
+ *
+ * Classe responsável por gerenciar a Conta de Laboratório, a qual é responsável por manter o registro da quantidade de
+ * espaço utilizado em determinado laboratório.
  */
 public class ContaLaboratorio {
     /**
      * Limite de cota em Megabytes.
      */
     private static final int LIMITE_COTA = 2000;
-
+    /**
+     * Contabiliza a cota utilizada no laboratório.
+     */
     private int cota;
+    /**
+     * Nome único do laboratório.
+     */
     private String nomeLaboratorio;
 
     /**
-     * Construtor da Conta do Laboratório.
-     * @param nomeLaboratorio
+     * Método do construtor da Conta do Laboratório.
+     * @param nomeLaboratorio Nome do Laboratório
      */
     public ContaLaboratorio(String nomeLaboratorio){
         this.nomeLaboratorio = nomeLaboratorio;
     }
 
     /**
-     * Construtor da Conta do Laboratório.
-     * @param nomeLaboratorio
-     * @param cota
+     * Método do construtor da Conta do Laboratório.
+     * @param nomeLaboratorio Nome do Laboratório deve ser único.
+     * @param cota Quantidade de cota utilizado.
      */
     public  ContaLaboratorio(String nomeLaboratorio, int cota){
         this.nomeLaboratorio = nomeLaboratorio;
@@ -33,8 +41,16 @@ public class ContaLaboratorio {
     }
 
     /**
+     * Retorna o nome do laborário.
+     * @return String corresponde ao nome do laboratório.
+     */
+    public String getNomeLaboratorio(){
+        return this.nomeLaboratorio;
+    }
+
+    /**
      * Registra o consumo de espaço no laboratório.
-     * @param megabytes
+     * @param megabytes Tamanho do consumo de espaço em Megabyte.
      */
     public void consomeEspaco(int megabytes){
         this.cota += megabytes;
@@ -42,14 +58,14 @@ public class ContaLaboratorio {
 
     /**
      * Libera espaço no laboratório.
-     * @param megabytes
+     * @param megabytes Tamanho do consumo de espaço em Megabyte.
      */
     public void liberaEspaco(int megabytes){
         this.cota -= megabytes;
     }
 
     /**
-     * Verifica se o laboratório atingiu a cota.
+     * Verifica se o laboratório atingiu a cota de consumo.
      * @return
      */
     public boolean atingiuCota(){
@@ -57,8 +73,8 @@ public class ContaLaboratorio {
     }
 
     /**
-     * Retorna informações do laboratório formatado.
-     * @return
+     * Retorna informações do laboratório formatado para uso.
+     * @return Incluindo (Nome do laboratório, conta e limte de cota)
      */
     public String toString(){
         return this.nomeLaboratorio + " " + this.cota + "/" + LIMITE_COTA;
